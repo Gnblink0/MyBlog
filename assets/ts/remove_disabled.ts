ec124c08dc720584f3115a80eb17132f28f9f96b
gnblink0 <gnblink44@gmail.com>
2024-02-04 14:23:23 -0800

    update

--- assets/ts/remove_disabled.ts
+++
@@ -1,7 +0,0 @@
-// 选择具有 disabled 属性的所有复选框
-const checkboxes: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[type="checkbox"][disabled]');
-
-// 移除这些复选框的 disabled 属性
-checkboxes.forEach(function(checkbox: HTMLInputElement) {
-  checkbox.removeAttribute('disabled');
-});
